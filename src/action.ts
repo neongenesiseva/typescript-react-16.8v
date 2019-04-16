9a1bf882325d14e36246d89f92f28e5cd218c3ba
Chris Li <rxl418@case.edu>
2019-04-16 13:04:34 -0400

    type and interface clarification

--- src/action.ts
+++ src/action.ts
@@ -10,12 +10,19 @@ export function addTodo(text: string) {
 
 // type is more like a tuple to represent a set of data, will not create a new name instance
 // interface is create a new name instance
-type DispatchParam = {
-    type: string,
-    payload?: any
-}
 
-type DispatchFunc = ({}: DispatchParam) => {}
+// below two both works
+
+// type DispatchParam = {
+//     type: string,
+//     payload?: any
+// }
+
+// type DispatchFunc = ({}: DispatchParam) => {}
+
+interface DispatchFunc {
+    ({}: {type: string, payload?: {}}): void
+}
 
 export function removeTodo(id: string) {
     return function (dispatch: DispatchFunc) {
